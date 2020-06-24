6b9a7ab8b6a1fe6dd902bafa844c6c3f52128247
Ben Szuchmacher <bszuchmacher@gmail.com>
2020-06-24 17:31:42 +0300

    milestones 2 and 3 finished. moving to 4 now

---
+++ js/company.js
@@ -0,0 +1,89 @@
+const urlParams = new URLSearchParams(window.location.search);
+const symbol = urlParams.get("symbol");
+
+async function getCompanyDetails() {
+  try {
+    const response = await fetch(
+      `https://financialmodelingprep.com/api/v3/company/profile/${symbol}?&apikey=ed93f3e229380c530b7a0e7663f86b99`
+    );
+    const data = await response.json();
+    // console.log(data); all the data from the symbol fetch.
+    displayCompanyData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function displayCompanyData(data) {
+  fetch(
+    `https://financialmodelingprep.com/api/v3/company/profile/${symbol}?&apikey=ed93f3e229380c530b7a0e7663f86b99`
+  )
+    .then((response) => response.json())
+    .then(function (data) {
+      const {
+        companyName,
+        industry,
+        price,
+        description,
+        image,
+        changesPercentage,
+        website,
+        sector,
+      } = data.profile;
+      document.getElementById("companyImage").src = image;
+      document.getElementById("companyName").textContent = companyName;
+      document.getElementById("companyName").setAttribute("href", `${website}`);
+      document.getElementById("industry").textContent = industry;
+      document.getElementById("sector").textContent = sector;
+      document.getElementById("companyPrice").textContent = `$ ${price}`;
+      document.getElementById("companyDescription").textContent = description;
+      document.getElementById("percentChange").textContent = changesPercentage;
+      if (changesPercentage.includes("+")) {
+        document.getElementById("percentChange").classList.add("green");
+      }
+      if (changesPercentage.includes("-")) {
+        document.getElementById("percentChange").classList.add("red");
+      }
+    });
+  //const name = data.profile.companyName;
+  // const imageUrl = data.profile.image;
+  // const description = data.profile.description;
+  // const companyUrl = data.profile.website;
+  // const price = data.profile.price;
+  // const stockChangePercent = data.profile.changesPercentage;
+  // const industry = data.profile.industry;
+}
+function fetchDataGraph() {
+  fetch(
+    `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?from=2020-06-12&to=2020-06-24&apikey=ed93f3e229380c530b7a0e7663f86b99`
+  ).then((response) =>
+    response.json().then((data) => {
+      //the array historical contains 2 items... date and closes
+      let historical = data.historical;
+      let dates = historical.map((companyfetched) => companyfetched.date);
+      xDates = dates.slice(Math.max(dates.length - 10, 0));
+      let closes = historical.map((companyfetched) => companyfetched.close);
+      yPrices = closes.slice(Math.max(closes.length - 10, 0));
+      createTheChart(xDates, yPrices);
+    })
+  );
+}
+function createTheChart(xDates, yPrices) {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: xDates,
+      datasets: [
+        {
+          label: `Price History of ${symbol}`,
+          backgroundColor: "rgba(155, 109, 132, 0.2)",
+          borderColor: "rgb(0 ,0, 0)",
+          data: yPrices,
+        },
+      ],
+    },
+  });
+}
+
+getCompanyDetails(symbol);
+fetchDataGraph();
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+const searchButton = document.getElementById("searchButton");
+
 function loaderOn() {
   document.getElementById("loader").classList.add("show");
 }
@@ -6,38 +8,70 @@ function loaderOff() {
   document.getElementById("loader").classList.add("hide");
 }
 
-function clearResult() {
-  document.getElementById("resultsList").innerHTML = " ";
-}
+function createResultsData(item) {
+  fetch(
+    `https://financialmodelingprep.com/api/v3/company/profile/${item.symbol}?apikey=ed93f3e229380c530b7a0e7663f86b99`
+  ).then((response) =>
+    response.json().then(function (data) {
+      // console.log(data);
+      let a = document.createElement("a");
+      document.getElementById("resultsList").appendChild(a);
+      a.classList.add("list-group-item");
+      a.setAttribute("href", `./company.html?symbol=${data.symbol}`);
 
-async function getInitialSearchResults() {
-  clearResult();
-  loaderOn();
-  const searchInput = document.getElementById("searchInput").value;
-  let response = await fetch(
-    `https://financialmodelingprep.com/api/v3/search?query=${searchInput}&limit=10&exchange=NASDAQ&apikey=ed93f3e229380c530b7a0e7663f86b99`
+      const image = document.createElement("img");
+      image.classList.add("companyLogo");
+      companyImage = data.profile.image;
+      image.src = companyImage;
+      a.appendChild(image);
+
+      const companyName = document.createElement("span");
+      companyName.classList.add("wordSpacing");
+      companyName.textContent = data.profile.companyName;
+      a.appendChild(companyName);
+
+      const symbol = document.createElement("span");
+      symbol.classList.add("wordSpacing");
+      symbol.innerText = "(" + data.symbol + ")";
+      a.appendChild(symbol);
+
+      const price = document.createElement("span");
+      price.classList.add("wordSpacing");
+      price.classList.add("black");
+      price.textContent = `   $${data.profile.price}   `;
+      a.appendChild(price);
+
+      const changesPercentage = document.createElement("span");
+      const percentChange = data.profile.changesPercentage;
+      changesPercentage.textContent = "(" + percentChange + ")";
+      if (percentChange.includes("+")) {
+        changesPercentage.classList.add("green");
+      } else if (percentChange.includes("-")) {
+        changesPercentage.classList.add("red");
+      } else {
+        changesPercentage.classList.add("black");
+      }
+      a.appendChild(changesPercentage);
+    })
   );
-  let data = await response.json();
-  createResults(data);
   loaderOff();
 }
 
-function createResults(data) {
-  const resultsList = document.getElementById("resultsList");
-  const companyList = data;
-  for (let i = 0; i < companyList.length; i++) {
-    const symbol = companyList[i].symbol;
-    let li = document.createElement("li");
-    let companyName = document.createElement("a");
-    companyName.setAttribute("href", `./company.html?symbol=${symbol}`);
-    companyName.innerText = companyList[i].name;
-    let companySymbol = document.createElement("a");
-    companyName.setAttribute("href", `./company.html?symbol=${symbol}`);
-    companySymbol.innerText = " (" + companyList[i].symbol + ")";
-    li.append(companyName, companySymbol);
-    resultsList.appendChild(li);
-  }
+function getInitialSearchResults(Input) {
+  let searchInput = document.getElementById("searchInput").value;
+  let url = `https://financialmodelingprep.com/api/v3/search?query=${searchInput}&limit=10&exchange=NASDAQ&apikey=ed93f3e229380c530b7a0e7663f86b99`;
+  // console.log(url);
+  // console.log(searchInput);
+  loaderOn();
+  fetch(url)
+    .then((response) => response.json())
+    .then(function (initialCompanyInfo) {
+      // console.log(url);
+      // console.log(initialCompanyInfo);//thats my array returned to me.
+      initialCompanyInfo.map((item) => {
+        //symbol and name accessed
+        createResultsData(item);
+      });
+    });
 }
-document
-  .getElementById("searchButton")
-  .addEventListener("click", getInitialSearchResults);
+searchButton.addEventListener("click", getInitialSearchResults);
